cd50dd76afbc8382f5dde99dcc0a01a17cda67fd
Hershal Bhave <hershal.bhave@gmail.com>
2014-12-08 16:16:14 -0600

    minor servlet cleanup; added json failure reason

--- src/com/supertictactoe/supertictactoe/PlayerMoveServlet.java
+++ src/com/supertictactoe/supertictactoe/PlayerMoveServlet.java
@@ -12,20 +12,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.thegreshams.firebase4j.error.FirebaseException;
-import net.thegreshams.firebase4j.error.JacksonUtilityException;
-import net.thegreshams.firebase4j.model.FirebaseResponse;
 import net.thegreshams.firebase4j.service.Firebase;
-
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
 
 import com.google.appengine.labs.repackaged.org.json.JSONException;
 import com.google.appengine.labs.repackaged.org.json.JSONObject;
-import com.ibm.icu.util.BytesTrie.Iterator;
 import com.supertictactoe.supertictactoe.components.*;
 
 import static com.supertictactoe.Common.StoredProcedure.*;
-import static com.supertictactoe.Common.SuperTicTacToeStrings.*;
 
 public class PlayerMoveServlet extends HttpServlet {
 
@@ -45,10 +38,19 @@ public class PlayerMoveServlet extends HttpServlet {
         final String id_inner_str = request.getParameter("id_inner");
 
         Move move = null;
+        JSONObject json = new JSONObject();
 
         if ((game_url == null) || (player == null) ||
             (id_outer_str == null) || (id_inner_str == null)) {
-
+            System.out.println("Received null data");
+            try {
+                /* Stupid java */
+                json.append("success", "false");
+                json.write(response.getWriter());
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
             return;
         }
         int id_outer = Integer.parseInt(id_outer_str);
@@ -60,9 +62,8 @@ public class PlayerMoveServlet extends HttpServlet {
 
         System.out.println(game_url + " " + move.toString());
 
-        /* create the firebase reference */
+        /* create the Firebase reference */
         try {
-            JSONObject json = new JSONObject();
             Firebase firebase = new Firebase(game_url);
 
             Game gm = SPParseFirebase(firebase);
@@ -75,16 +76,34 @@ public class PlayerMoveServlet extends HttpServlet {
             } else {
                 System.out.println("INVALID MOVE IGNORED!");
                 json.append("success", "false");
-                /* Send response that move is not valid */
+                json.append("reason", "invalid_move");
             }
             json.write(response.getWriter());
         } catch (FirebaseException e) {
             /* STUB */
             e.printStackTrace();
+            try {
+                /* Stupid java */
+                json.append("success", "false");
+                json.append("reason", "firebase_exception");
+                json.write(response.getWriter());
+            } catch (JSONException jse) {
+                // TODO Auto-generated catch block
+                jse.printStackTrace();
+            }
         } catch (JSONException e) {
             // This is stupid; adding to a JSON object is NOT exceptional!
             // TODO Auto-generated catch block
             e.printStackTrace();
+            try {
+                /* Stupid java */
+                json.append("success", "false");
+                json.append("reason", "json_exception");
+                json.write(response.getWriter());
+            } catch (JSONException jse) {
+                // TODO Auto-generated catch block
+                jse.printStackTrace();
+            }
         }
     }
 }
